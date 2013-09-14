d9422066ceef930957b2995af379b1abbdd73230
Basil Huang <Basil@Basils-MacBook-Air.local>
2013-09-14 10:13:24 -0700

    updates to spreadsheet

--- coupon-nesting.py
+++ coupon-nesting.py
@@ -7,22 +7,26 @@ import Tkinter, tkFileDialog
 # of the spreadsheet. Then the spreadsheet is opened up and the spreadsheet 
 # object is returned.
 def openSpreadsheet():
-	root = Tkinter.Tk()
-	root.withdraw()
-	file_path = tkFileDialog.askopenfilename()
-	book = xlrd.open_workbook(file_path)
-	return book.sheet_by_index(0)
+    root = Tkinter.Tk()
+    root.withdraw()
+    file_path = tkFileDialog.askopenfilename()
+    book = xlrd.open_workbook(file_path)
+    return book.sheet_by_index(0)
 
 # Input: spreadsheet
 # Output: a dictionary containing the spreadsheet information
 # This function will create the dictionary containing the trim edge, blade
 # width, tolerance, 
 def parseExcel(sheet):
-    spreadsheetDictionary = {'trimEdge': 1, 'bladeWidth': 0.07, 'tolerance': 0.015}
-    for i in range(18):
-        currentCell = sh.cell_value(i,0)
+    spreadsheetDictionary = {}
+    for i in range(sheet.nrow):
+        currentCell = sheet.cell_value(i,0)
+        # At defaults header
+        if (currentCell == 'Defaults') and (sheet.cell_value(i, 1) == xlrd.empty_cell.value):
+        	spreadsheetDictionary['bladeWidth'] = sheet.cell_value(i+1, 1)
+        	spreadsheetDictionary['tolerance'] = sheet.cell_value(i+2, 1)
+        	i+=2
+        elif (currentCell == 'Panel'):
 
-
-
-
-
+        elif (currentCell == xlrd.empty_cell.value):
+        	continue
