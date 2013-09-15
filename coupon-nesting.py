e3736919231965d71c91c91c7a1faf460bc2a3df
Basil Huang <Basil@Basils-MacBook-Air.local>
2013-09-15 00:21:48 -0700

    some more parsing logic

--- coupon-nesting.py
+++ coupon-nesting.py
@@ -1,19 +1,37 @@
 import xlrd
 import Tkinter, tkFileDialog
 
-class RectangularCoupon:
+class Coupon:
     def __init__(self):
         self.couponSeries
-        self.width
-        self.length
-        self.quantity
         self.bladeWidth
-        self.area
+        self.quantity
+
+class RectangularCoupon(Coupon):
+    def __init__(self, couponSeries, quantity, length, width, bladeWidth):
+        self.couponSeries = couponSeries
+        self.quantity = quantity
+        self.width = width
+        self.length = length
+        self.bladeWidth = bladeWidth
+        self.area = length*width + (length+width)*bladeWidth #simple area calculation to account for bladeWidth
         self.x0
         self.y0
         self.x1
         self.y1
 
+class CircularCoupon(Coupon):
+    def __init__(self, couponSeries, quantity, radius, bladeWidth):
+        self.couponSeries = couponSeries
+        self.quantity = quantity
+        self.radius = radius
+        self.diameter = radius << 1
+        self.bladeWidth = bladeWidth
+        self.area = self.diameter * self.diameter + (self.diameter + self.diameter)*bladeWidth
+        self.x0
+        self.y0
+        self.x1
+        self.y1
 
 # Input: none
 # Output: spreadsheet 
@@ -29,8 +47,8 @@ def openSpreadsheet():
 
 # Input: spreadsheet, sectionFlag
 # Output: the information for the row parsed
-def extractRowData(sheet, sectionFlag):
-
+# def extractRowData(sheet, sectionFlag):
+#     if sectionFlag
 # Input: spreadsheet
 # Output: a dictionary containing the spreadsheet information
 # This function will create the dictionary containing the trim edge, blade
@@ -38,8 +56,11 @@ def extractRowData(sheet, sectionFlag):
 def parseExcel(sheet):
     spreadsheetDictionary = {}
     sectionFlag = 'defaults'
-    for i in range(sheet.nrow):
+    for i in range(sheet.nrows):
+        print "{0} / {1} ".format(i, sheet.nrows)
         currentCell = sheet.cell_value(i,0)
+        print currentCell
+        raw_input('Check the current cell above')
         # At defaults header
         if (sectionFlag == 'defaults'):
         	spreadsheetDictionary['bladeWidth'] = sheet.cell_value(i+1, 1)
@@ -47,15 +68,23 @@ def parseExcel(sheet):
         	i+=2
         elif (currentCell == 'Panel'):
             sectionFlag = 'panel'
-            continue
+            pLength = sheet.cell_value(i+1, 0)
+            pWidth = sheet.cell_value(i+1, 1)
+            pTrimEdge = sheet.cell_value(i+1, 2)
+            panelDimensions = {'pLength':pLength, 'pWidth':pWidth, 'pTrimEdge':pTrimEdge}
+            spreadsheetDictionary['panelDimensions'] = panelDimensions
+            i+=1
         elif (currentCell == 'Rectangular Coupons'):
             sectionFlag = 'rectCoupons'
         elif (currentCell == "Circiular Coupons"):
             sectionFlag = 'circularCoupons'
         elif (currentCell == xlrd.empty_cell.value):
         	continue
-        else:
-            extractRowData(sheet, sectionFlag)
+        else: #we are parsing through the data for rectangular/circular coupons
+            #extractRowData(sheet, sectionFlag)
+            print "Currently done."
+
+        return spreadsheetDictionary
 
 
 
