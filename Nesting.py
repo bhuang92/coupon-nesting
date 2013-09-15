02eeceaf51d907cea8cb22f144f21d63e678e393
Basil Huang <Basil@Basils-MacBook-Air.local>
2013-09-15 12:15:03 -0700

    finished handling panel and defaults, begin working on coupons

--- Nesting.py
+++ Nesting.py
@@ -12,6 +12,12 @@ class Coupon:
     x1 = 0
     y1 = 0
 
+class Panel:
+    def __init__(self, length, width, trimEdge):
+        self.length = length
+        self.width = width
+        self.trimEdge = trimEdge
+
 # This is the extension of Coupon
 class RectangularCoupon(Coupon):
     def __init__(self, couponSeries, quantity, length, width, bladeWidth):
@@ -44,45 +50,109 @@ def openSpreadsheet():
     book = xlrd.open_workbook(file_path)
     return book.sheet_by_index(0)
 
+# Input: anything
+# Output: boolean indicating whether s is a number
+def isNumber(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+# Input: spreadsheet, row, spreadsheetDictionary
+# Output: updated spreadsheetDictionary
+def getDefaults(sheet, curr, row, ssd):
+    if curr == 'blade width' and isNumber(sheet.cell_value(row,1)):
+        ssd['bladeWidth'] = sheet.cell_value(row, 1)
+    elif curr == 'tolerance' and isNumber(sheet.cell_value(row,1)):
+        ssd['tolerance'] = sheet.cell_value(row, 1)
+    elif not isNumber(sheet.cell_value(row,1)):
+        return False
+    return ssd
+
+# Input: spreadsheet, row, spreadsheetDictionary
+# Output: updated spreadsheetDictionary / false
+def getPanelDimensions(sheet, row, ssd):
+    panelValid = True
+    # Check that length, width, and trim edge are all numbers
+    for i in range(3):
+        if not isNumber(sheet.cell_value(row, i)):
+            panelValid = False
+    if panelValid:
+        pLength = sheet.cell_value(row, 0)
+        pWidth = sheet.cell_value(row, 1)
+        pTrimEdge = sheet.cell_value(row, 2)
+        panel = Panel(pLength, pWidth, pTrimEdge)
+        ssd['panel'] = panel
+        return ssd
+    else:
+        return False
+
+# Input: currentCell
+# Output: sectionFlag
+def getSectionFlag(currentCell):
+    if (currentCell == 'Defaults'):
+        sectionFlag = 'defaults'
+    elif (currentCell == 'Panel'):
+        sectionFlag = 'panel'
+    elif (currentCell == 'Rectangular Coupons'):
+        sectionFlag = 'rectangularCoupons'
+    elif (currentCell == 'Circular Coupons'):
+        sectionFlag = 'circularCoupons'
+    elif (currentCell == xlrd.empty_cell.value):
+        sectionFlag = 'break'
+    return sectionFlag
+
+
 # Input: spreadsheet, sectionFlag
 # Output: the information for the row parsed
 # def extractRowData(sheet, sectionFlag):
 #     if sectionFlag
+
 # Input: spreadsheet
 # Output: a dictionary containing the spreadsheet information
-# This function will create the dictionary containing the trim edge, blade
-# width, tolerance, 
+# This function will create the dictionary containing the blade
+# width, tolerance, and information about panels and coupons.
 def parseExcel(sheet):
     spreadsheetDictionary = {}
     sectionFlag = 'defaults'
     for i in range(sheet.nrows):
         print "{0} / {1} ".format(i, sheet.nrows)
         currentCell = sheet.cell_value(i,0)
+        sectionFlag = getSectionFlag(currentCell)
+
         print currentCell
+        print "spreadsheetDictionary so far: {0}".format(spreadsheetDictionary)
+        print "Section flag : {0}".format(sectionFlag)
         raw_input('Check the current cell above')
-        # At defaults header
+
+        continue # Go to next row to avoid having to handle extra bad cases
+
         if (sectionFlag == 'defaults'):
-            spreadsheetDictionary['bladeWidth'] = sheet.cell_value(i+1, 1)
-            spreadsheetDictionary['tolerance'] = sheet.cell_value(i+2, 1)
-            i+=2
-        elif (currentCell == 'Panel'):
-            sectionFlag = 'panel'
-            pLength = sheet.cell_value(i+1, 0)
-            pWidth = sheet.cell_value(i+1, 1)
-            pTrimEdge = sheet.cell_value(i+1, 2)
-            panelDimensions = {'pLength':pLength, 'pWidth':pWidth, 'pTrimEdge':pTrimEdge}
-            spreadsheetDictionary['panelDimensions'] = panelDimensions
-            i+=1
-        elif (currentCell == 'Rectangular Coupons'):
-            sectionFlag = 'rectCoupons'
-        elif (currentCell == "Circiular Coupons"):
-            sectionFlag = 'circularCoupons'
-        elif (currentCell == xlrd.empty_cell.value):
+            defaults = getDefaults(sheet, curr, i, spreadsheetDictionary):
+            if defaults: # if valid
+                ssd = defaults
+            else:
+                raise Exception("Defaults must be numbers.")
+        elif (sectionFlag == 'break'):
             continue
-        else: #we are parsing through the data for rectangular/circular coupons
-            #extractRowData(sheet, sectionFlag)
-            print "Currently done."
+        elif (sectionFlag == 'panel'):
+            panelDim = getPanelDimensions(sheet, i, spreadsheetDictionary)
+            if panelDim: # if valid
+                ssd = panelDim
+            else:
+                raise Exception("Panel dimensions must be numbers.")
+        elif (sectionFlag == 'rectangularCoupons'): #we are parsing through the data for rectangular/circular coupons
+            print "rectangular coupons."
+        elif (sectionFlag == 'circularCoupons'):
+            print "circular coupons."
     return spreadsheetDictionary
+
+
+
+
+
+
 
 
 
